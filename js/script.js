2f6ae8253bef4613bf55e337fae5339792077848
Guy Roger Eboulet <eboulet87@gmail.com>
2020-01-09 16:48:51 +0100

    Elenco corretto

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 
-// 1) Scrivi un programma che stampi i numeri da 1 a 100,
-for (var i = 1; i <= 1000; i++) {
-  // 2) ma per i multipli di 3 stampi “Fizz” al posto del numero e
-  if (i%3 == 0) {
-    console.log(i);
+for (var i = 1; i <= 100; i++) {
+
+  if ((i%5 == 0)&&(i%3 == 0)) {
+    console.log( "fizzbuzz");
   }
-  // 3)per i multipli di 5 stampi Buzz.
+  else if (i%3 == 0) {
+    console.log("fizz");
+  }
   else if (i%5 == 0) {
-    console.log(i);
+    console.log("buzz");
   }
-  // 4)Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-  else if ((i%5 == 0)&&(i%3 == 0)) {
-    console.log();
-  }
+
+  console.log(i);
+
 }
