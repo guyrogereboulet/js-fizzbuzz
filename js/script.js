9af460962535be782faef45d0e21ec8c62ffd813
Guy Roger Eboulet <eboulet87@gmail.com>
2020-01-09 17:36:18 +0100

    Commit finale

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 
 for (var i = 1; i <= 100; i++) {
   if ((i%5 == 0)&&(i%3 == 0)) {
-    console.log( "fizzbuzz");
+    console.log("fizzbuzz");
+    document.getElementById("lista").innerHTML += "fizzbuzz" + "<br>";
   }
   else if (i%3 == 0) {
     console.log("fizz");
+    document.getElementById("lista").innerHTML += "fizz" + "<br>";
   }
   else if (i%5 == 0) {
     console.log("buzz");
+    document.getElementById("lista").innerHTML += "buzz" + "<br>";
   }
   else {
     console.log(i);
+    document.getElementById("lista").innerHTML += i + "<br>";
   }
 
 }
