87645c10c876b32aa78bc63f3ffe24acda99e909
Guy Roger Eboulet <eboulet87@gmail.com>
2020-01-09 17:04:38 +0100

    Condizione residuale

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 
 for (var i = 1; i <= 100; i++) {
-
   if ((i%5 == 0)&&(i%3 == 0)) {
     console.log( "fizzbuzz");
   }
@@ -10,7 +9,8 @@ for (var i = 1; i <= 100; i++) {
   else if (i%5 == 0) {
     console.log("buzz");
   }
-
-  console.log(i);
+  else {
+    console.log(i);
+  }
 
 }
